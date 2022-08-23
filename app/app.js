55741d45eddce89a702eff3b84b40d332f8bd79c
Alexandr <44476785+Gaijira@users.noreply.github.com>
2022-08-23 18:39:15 +0300

    add dir

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 //DB Connection
 mongoose.connect(`mongodb+srv://${ process.env.DB_LOGIN }:${ process.env.DB_PASSWORD }@cluster0.ro8bz.mongodb.net/dogs_db?retryWrites=true&w=majority`)
-  .then((res) => {console.log('\nConnected to DB')})
+  .then((res) => {console.log('\nConnected to DB.')})
   .catch((err) => {console.log(err)})
 
 app.use(express.json())
